a7cd751b36fba91ac90961f7feabb75256055304
idejaferati <idejaferati03@gmail.com>
2021-11-12 19:17:58 +0100

    Worked on header and menu.

---
+++ js/header_footer.js
@@ -0,0 +1,12 @@
+window.onload = function ()
+{
+    var header_content = '<div class="header-div"><a href="index.html"><div class="main-logo"></div></a></div>';
+    var footer_content = '';
+
+    var header_element = document.getElementById('header');
+    var footer_element = document.getElementById('footer');
+
+    header_element.innerHTML = header_content;
+    footer_element.innerHTML = footer_content;
+
+}
